b9c94aeb45c7f0edf0d3f133b294babf89c633e6
Sai Sreekar <siddula.s@northeastern.edu>
2020-08-23 15:36:32 -0400

    Updates

---
+++ precache-manifest.83a6ab444cbe3210ca74977519318cc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f65df34ee861b1ebe76c83d6c7b8e995",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b543a27e7c179f661ad",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8e50852c89055bb339e0",
+    "url": "/static/js/2.034589ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.034589ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b543a27e7c179f661ad",
+    "url": "/static/js/main.5391f453.chunk.js"
+  },
+  {
+    "revision": "acfd49d55fc918960123",
+    "url": "/static/js/runtime-main.788275b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
