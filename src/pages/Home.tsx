f6e2c6580645901703410ef8fcd7c5ee3be2baf8
Sreekar Siddula <sai06sreekar@gmail.com>
2024-07-13 19:51:55 -0400

    added publications section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import About from "./About";
 import Experience from "./Experience";
 import Education from "./Education";
 import Projects from "./Projects";
+import Publications from "./Publications";
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
       <Experience />
       <Education />
       <Projects />
+      <Publications />
     </div>
   );
 }
